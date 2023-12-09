fa9d2a9cb558e1a2ee42b72fe31d221fca9b7296
ruben <ruben@zukic.net>
2023-12-09 14:02:33 +0100

    removed deletion to aid syncing with other calendar apps

--- cron.js
+++ cron.js
@@ -8,53 +8,113 @@ function cron_job() {
     title: event.getTitle(),
     start: event.getStartTime(),
     end: event.getEndTime(),
+    location: event.getLocation(),
   }));
+
+  const typeMap = {
+    sa: 'sa',
+    swh: 'swh',
+    smü: 'swh',
+    test: 'swh',
+    praklf: 'plf',
+  };
 
   const stundenplan = CalendarApp.getCalendarsByName('Stundenplan')[0];
   const swh = CalendarApp.getCalendarsByName('SWH')[0];
   const sa = CalendarApp.getCalendarsByName('SA')[0];
   const plf = CalendarApp.getCalendarsByName('PLF')[0];
 
-  let oldEvents = stundenplan.getEvents(new Date(), inAMonth);
-  oldEvents = oldEvents.concat(swh.getEvents(new Date(), inAMonth));
-  oldEvents = oldEvents.concat(sa.getEvents(new Date(), inAMonth));
-  oldEvents = oldEvents.concat(plf.getEvents(new Date(), inAMonth));
+  const calendarMap = {
+    normal: stundenplan,
+    swh: swh,
+    sa: sa,
+    plf: plf,
+  };
+
+  let oldEvents = stundenplan.getEvents(new Date(), inAMonth).map((event) => ({
+    event: event,
+    start: event.getStartTime(),
+    end: event.getEndTime(),
+    type: 'normal',
+    title: event.getTitle(),
+    location: event.getLocation(),
+  }));
+  oldEvents = oldEvents.concat(
+    swh.getEvents(new Date(), inAMonth).map((event) => ({
+      event: event,
+      start: event.getStartTime(),
+      end: event.getEndTime(),
+      type: 'swh',
+      title: event.getTitle(),
+      location: event.getLocation(),
+    }))
+  );
+  oldEvents = oldEvents.concat(
+    sa.getEvents(new Date(), inAMonth).map((event) => ({
+      event: event,
+      start: event.getStartTime(),
+      end: event.getEndTime(),
+      type: 'sa',
+      title: event.getTitle(),
+      location: event.getLocation(),
+    }))
+  );
+  oldEvents = oldEvents.concat(
+    plf.getEvents(new Date(), inAMonth).map((event) => ({
+      event: event,
+      start: event.getStartTime(),
+      end: event.getEndTime(),
+      type: 'plf',
+      title: event.getTitle(),
+      location: event.getLocation(),
+    }))
+  );
+
+  events = events.map((obj) => {
+    let type = 'normal';
+    let title = obj.title;
+    if (obj.title.startsWith('[')) {
+      let strs = obj.title.slice(1).split(']');
+      type = typeMap[strs[0].toLocaleLowerCase()];
+      title = strs[1].trim();
+    }
+    return {
+      event: obj.event,
+      start: obj.start,
+      end: obj.end,
+      type: type,
+      title: title,
+      location: obj.location,
+    };
+  });
 
   for (const event of oldEvents) {
-    event.deleteEvent();
-  }
-
-  const specialEvents = events
-    .filter((e) => e.title.startsWith('['))
-    .map((obj) => {
-      let strs = obj.title.slice(1).split(']');
-      return {
-        event: obj.event,
-        type: strs[0].toLowerCase(),
-        title: strs[1].trim(),
-      };
-    });
-  events = events.filter((e) => !e.title.startsWith('['));
-
-  for (const ev of events) {
-    let start = ev.event.getStartTime();
-    let end = ev.event.getEndTime();
-    stundenplan.createEvent(ev.title, start, end, { location: ev.event.getLocation() });
-  }
-
-  for (const ev of specialEvents) {
-    if (ev.type == 'sa') {
-      let start = ev.event.getStartTime();
-      let end = ev.event.getEndTime();
-      sa.createEvent(ev.title, start, end, { location: ev.event.getLocation() });
-    } else if (ev.type == 'swh' || ev.type == 'smü' || ev.type == 'test') {
-      let start = ev.event.getStartTime();
-      let end = ev.event.getEndTime();
-      swh.createEvent(ev.title, start, end, { location: ev.event.getLocation() });
-    } else if (ev.type == 'praklf') {
-      let start = ev.event.getStartTime();
-      let end = ev.event.getEndTime();
-      plf.createEvent(ev.title, start, end, { location: ev.event.getLocation() });
+    const newEvents = events.filter((e) => e.start.getTime() == event.start.getTime() && e.end.getTime() == event.end.getTime());
+    if (newEvents.length == 0) {
+      event.event.deleteEvent();
+    } else if (newEvents.length == 1) {
+      let newEvent = newEvents[0];
+      events.splice(events.indexOf(newEvent), 1);
+      if (newEvent.type == event.type) {
+        refreshEventProperties(event, newEvent);
+      } else {
+        event.event.deleteEvent();
+        calendarMap[newEvent.type].createEvent(newEvent.title, newEvent.start, newEvent.end, { location: newEvent.location });
+      }
     }
+  }
+  for (const event of events) {
+    calendarMap[event.type].createEvent(event.title, event.start, event.end, { location: event.location });
+  }
+}
+
+function refreshEventProperties(oldEvent, newEvent) {
+  let o = oldEvent.event;
+  let n = newEvent.event;
+  if (newEvent.title != oldEvent.title) {
+    o.setTitle(newEvent.title);
+  }
+  if (newEvent.location != oldEvent.location) {
+    o.setLocation(n.getLocation());
   }
 }
